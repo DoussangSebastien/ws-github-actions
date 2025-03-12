04bb0ccde83d1a3b03e7aed1252c227bc786fdfe
Sébastien Doussang <sebastien.doussang@epitech.eu>
2025-03-12 15:13:00 +0100

    fix

--- src/main.c
+++ src/main.c
@@ -10,12 +10,6 @@
 
 int main(const int ac, char **av)
 {
-    for (int i = 0; i < 10; i++) {
-        for (int i = 0; i < 10; i++) {
-            for (int i = 0; i < 10; i++)
-                printf("toto\n");
-        }
-    }
     if (ac != 2 || av == NULL || av[1] == NULL)
         return EPITECH_KO;
     return swapupcase(av[1]);
